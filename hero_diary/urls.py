a8d0198ca7b8de1ae22c00ba80b9570750a7839d
Ynush <januszjanowski@outlook.com>
2020-06-07 12:46:28 +0200

    add last_action_time

--- hero_diary/urls.py
+++ hero_diary/urls.py
@@ -1,10 +1,11 @@
 from django.contrib import admin
 from django.urls import path
 
-from diary.views import StartView, CreateHero
+from diary.views import StartView, CreateHero, CheckHero
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('create_hero', CreateHero.as_view(), name='create-hero'),
+    path('view_hero/<int:pk>', CheckHero.as_view, name='hero'),
     path('', StartView.as_view(), name='index'),
 ]
